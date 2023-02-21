ed3ad5d9ace197cde25cd29b0a17f3708cfe0b44
kevienotieno <kevienotieno@gmail.com>
2023-02-21 11:27:43 -0800

    rdf

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: Always 0 (Success)
+ */
 /**
  * times_table - prints the 9 times table, starting with 0
  */
