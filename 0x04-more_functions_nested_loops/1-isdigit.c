402397b86a7a538532f449a810021b095f755970
kevienotieno <kevienotieno@gmail.com>
2023-02-23 12:18:54 -0800

    nested

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,5 @@
-#include "holberton.h"
-#include <stdio.h>
-
 #include "main.h"
 /**
- * _isdigit - Check if paramter c represents an digit.
- * @c: Int representing an ascii character
- * Return: Return 1 if it is a digit otherwise return 0.
  * _isdigit - Check for a digit (0-9)
  * @c: Integer holding the character
  *
@@ -15,10 +9,7 @@
 int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
-	{
 		return (1);
-	}
 
 	return (0);
-}
 }
